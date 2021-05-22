51aa948e865c51dccec3f1e594339f1c5de13830
Nectarios Chroniaris <nchroniaris@gmail.com>
2021-05-21 18:38:50 -0600

    initial project commit

---
+++ src/main/java/com/nchroniaris/signstonks/SignStonks.java
@@ -0,0 +1,12 @@
+package com.nchroniaris.signstonks;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SignStonks extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        super.getLogger().info("Hello World!");
+    }
+
+}
