282f9905f4f7e260a8ab3f64812f9b703e0e47bb
Nectarios Chroniaris <nchroniaris@gmail.com>
2021-06-25 02:52:39 -0600

    add config.yml with default generation
    - This makes validatePluginFolder() useless since JavaPlugin#saveDefaultConfig() will generate the folder if it DNE

--- src/main/java/com/nchroniaris/signstonks/SignStonks.java
+++ src/main/java/com/nchroniaris/signstonks/SignStonks.java
@@ -4,11 +4,7 @@ import com.nchroniaris.signstonks.command.RegisterCommandExecutor;
 import org.bukkit.command.PluginCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.io.IOException;
-import java.nio.file.FileAlreadyExistsException;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.logging.Level;
 
 public final class SignStonks extends JavaPlugin {
 
@@ -23,40 +19,14 @@ public final class SignStonks extends JavaPlugin {
     @Override
     public void onEnable() {
 
-        super.getLogger().info("Initializing...");
+        this.getLogger().info("Initializing...");
 
-        // Makes sure the data folder exists
-        this.validatePluginFolder();
+        // Saves the default config if does not exist, failing silently if it does
+        // Also ensures that the plugin's data folder exists
+        this.saveDefaultConfig();
 
         this.registerCommands();
         this.registerListeners();
-
-//        super.getServer().getPluginManager().registerEvents(new TestListener(), this);
-
-    }
-
-    /**
-     * This makes sure the plugin's data folder exists and is a folder. If it does not exist then it creates it. However, if it's anything but a directory, the plugin will fail to load to begin with and this code will not be run.
-     */
-    private void validatePluginFolder() throws IllegalStateException {
-
-        if (!Files.exists(this.pathDataFolder)) {
-
-            try {
-
-                Files.createDirectory(this.pathDataFolder);
-
-            } catch (FileAlreadyExistsException ignored) {
-            } catch (IOException e) {
-
-                RuntimeException pluginException = new IllegalStateException("Could not create data folder!");
-                pluginException.addSuppressed(e);
-
-                throw pluginException;
-
-            }
-
-        }
 
     }
 
@@ -76,6 +46,8 @@ public final class SignStonks extends JavaPlugin {
      * Registers all the listeners with the server
      */
     private void registerListeners() {
+
+        this.getServer().getPluginManager().registerEvents(new TestListener(), this);
 
     }
 
