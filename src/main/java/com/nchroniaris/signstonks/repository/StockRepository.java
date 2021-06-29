cbe8d4de637c5ac6df3f2614719b032703ed613d
Nectarios Chroniaris <nchroniaris@gmail.com>
2021-06-29 01:10:02 -0600

    add contracts for StockRepository and stub methods

--- src/main/java/com/nchroniaris/signstonks/repository/StockRepository.java
+++ src/main/java/com/nchroniaris/signstonks/repository/StockRepository.java
@@ -1,8 +1,113 @@
 package com.nchroniaris.signstonks.repository;
 
+import org.bukkit.Location;
+
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
 /**
  * Represents a contract for a repository class that implements methods to interact with a storage for {@link com.nchroniaris.mcstonks.stock.Stock}s
  */
 public interface StockRepository extends ConfigurationRepository {
+
+    /**
+     * Gets all "transaction signs" for a specific stock.
+     * <p>
+     * A "transaction sign" is a sign that is responsible for buying and selling {@link com.nchroniaris.mcstonks.stock.Stock}s to the player.
+     *
+     * @param stock The {@link UUID} of the {@link com.nchroniaris.mcstonks.stock.Stock} that you want to get the list for
+     * @return A {@link List} of {@link Location}s, each location referencing a (hopefully still) valid SignShop sign. This will return an empty list if there are no locations in the storage OR the UUID does not exist.
+     */
+    List<Location> getTransactionSigns(UUID stock);
+
+    /**
+     * Gets all "history signs" for a specific stock.
+     * <p>
+     * A "history sign" is a sign that is responsible for displaying a number of historical prices of a specific {@link com.nchroniaris.mcstonks.stock.Stock}.
+     *
+     * @param stock The {@link UUID} of the {@link com.nchroniaris.mcstonks.stock.Stock} that you want to get the list for
+     * @return A {@link List} of {@link Location}s, each location referencing a sign (the signs pointed to by these locations are nothing special). This will return an empty list if there are no locations in the storage OR the UUID does not exist.
+     */
+    List<Location> getHistorySigns(UUID stock);
+
+    /**
+     * Gets all the {@link com.nchroniaris.mcstonks.stock.Stock} {@link UUID}s present in the storage.
+     *
+     * @return A {@link Set} of {@link UUID}s, each representing a {@link com.nchroniaris.mcstonks.stock.Stock}. This will return an empty list if there are no locations in the storage OR the UUID does not exist.
+     */
+    Set<UUID> getStockEntries();
+
+    /**
+     * Adds a transaction sign to the list for the {@link com.nchroniaris.mcstonks.stock.Stock} specified by the {@link UUID}.
+     *
+     * @param stock    The {@link UUID} of the {@link com.nchroniaris.mcstonks.stock.Stock} that you want to add to
+     * @param location The {@link Location} of the Sign that you want to add. There are no checks for if this location has a valid sign.
+     */
+    void addTransactionSign(UUID stock, Location location);
+
+    /**
+     * Adds a history sign to the list for the {@link com.nchroniaris.mcstonks.stock.Stock} specified by the {@link UUID}.
+     *
+     * @param stock    The {@link UUID} of the {@link com.nchroniaris.mcstonks.stock.Stock} that you want to add to
+     * @param location The {@link Location} of the Sign that you want to add. There are no checks for if this location has a valid sign.
+     */
+    void addHistorySign(UUID stock, Location location);
+
+    /**
+     * Deletes a transaction sign from the list for the {@link com.nchroniaris.mcstonks.stock.Stock} specified by the {@link UUID}.
+     * <p>
+     * This will delete the first occurrence only (if exists), so duplicate entries (which are not technically valid) must be deleted with subsequent calls.
+     *
+     * @param stock    The {@link UUID} of the {@link com.nchroniaris.mcstonks.stock.Stock} that you want to remove from
+     * @param location The {@link Location} of the Sign that you want to remove.
+     * @return {@code true} if the deletion was successful, {@code false} otherwise
+     */
+    boolean deleteTransactionSign(UUID stock, Location location);
+
+    /**
+     * Deletes a history sign from the list for the {@link com.nchroniaris.mcstonks.stock.Stock} specified by the {@link UUID}.
+     * <p>
+     * This will delete the first occurrence only (if exists), so duplicate entries (which are not technically valid) must be deleted with subsequent calls.
+     *
+     * @param stock    The {@link UUID} of the {@link com.nchroniaris.mcstonks.stock.Stock} that you want to remove from
+     * @param location The {@link Location} of the Sign that you want to remove.
+     * @return {@code true} if the deletion was successful, {@code false} otherwise
+     */
+    boolean deleteHistorySign(UUID stock, Location location);
+
+    /**
+     * Deletes a transaction sign from the list for the {@link com.nchroniaris.mcstonks.stock.Stock} specified by its {@link UUID}.
+     * <p>
+     * This will delete the first occurrence only (if exists), so duplicate entries (which are not technically valid) must be deleted with subsequent calls.
+     * <p>
+     * This method is broader but slower to execute, as it needs to search through all locations saved. Useful if you don't have a UUID.
+     *
+     * @param location The {@link Location} of the Sign that you want to remove.
+     * @return {@code true} if the deletion was successful, {@code false} otherwise
+     */
+    boolean deleteTransactionSign(Location location);
+
+    /**
+     * Deletes a history sign from the list for the {@link com.nchroniaris.mcstonks.stock.Stock} specified by its {@link UUID}.
+     * <p>
+     * This will delete the first occurrence only (if exists), so duplicate entries (which are not technically valid) must be deleted with subsequent calls.
+     * <p>
+     * This method is broader but slower to execute, as it needs to search through all locations saved. Useful if you don't have a UUID.
+     *
+     * @param location The {@link Location} of the Sign that you want to remove.
+     * @return {@code true} if the deletion was successful, {@code false} otherwise
+     */
+    boolean deleteHistorySign(Location location);
+
+    /**
+     * Delete the entry specified by the parameter from the storage. This is designed to be used to clean up unused entries in the configuration.
+     * <p>
+     * <b>This will delete all associated transaction and history signs!</b>
+     *
+     * @param stock The UUID of the entry you want to remove
+     */
+    boolean deleteStockEntry(UUID stock);
+
 
 }
--- src/main/java/com/nchroniaris/signstonks/repository/concrete/StockRepositoryYAML.java
+++ src/main/java/com/nchroniaris/signstonks/repository/concrete/StockRepositoryYAML.java
@@ -1,10 +1,11 @@
 package com.nchroniaris.signstonks.repository.concrete;
 
 import com.nchroniaris.signstonks.repository.StockRepository;
+import org.bukkit.Location;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Objects;
+import java.util.*;
 
 /**
  * YAML-based repository for storing {@link com.nchroniaris.mcstonks.stock.Stock} objects
@@ -24,6 +25,56 @@ public class StockRepositoryYAML extends ConfigurationRepositoryYAML implements 
                         .getDataFolder().toPath().resolve("stocks.yml")
         );
 
+    }
+
+    @Override
+    public List<Location> getTransactionSigns(UUID stock) {
+        return null;
+    }
+
+    @Override
+    public List<Location> getHistorySigns(UUID stock) {
+        return null;
+    }
+
+    @Override
+    public Set<UUID> getStockEntries() {
+        return null;
+    }
+
+    @Override
+    public void addTransactionSign(UUID stock, Location location) {
+
+    }
+
+    @Override
+    public void addHistorySign(UUID stock, Location location) {
+
+    }
+
+    @Override
+    public boolean deleteTransactionSign(UUID stock, Location location) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteHistorySign(UUID stock, Location location) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteTransactionSign(Location location) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteHistorySign(Location location) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteStockEntry(UUID stock) {
+        return false;
     }
 
 }
