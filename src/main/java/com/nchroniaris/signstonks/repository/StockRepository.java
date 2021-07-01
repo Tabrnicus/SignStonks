59f7eefcc5646dc63ced4a537586b8e0b727880c
Nectarios Chroniaris <nchroniaris@gmail.com>
2021-06-30 18:49:08 -0600

    implement rest of Repository contract
    - rename `stock` to `stockUUID` to be more descriptive
    - utilize functional interfaces to abstract common code

--- src/main/java/com/nchroniaris/signstonks/repository/StockRepository.java
+++ src/main/java/com/nchroniaris/signstonks/repository/StockRepository.java
@@ -19,22 +19,22 @@ public interface StockRepository extends ConfigurationRepository {
      * <p>
      * A "transaction sign" is a sign that is responsible for buying and selling {@link com.nchroniaris.mcstonks.stock.Stock}s to the player.
      *
-     * @param stock The {@link UUID} of the {@link com.nchroniaris.mcstonks.stock.Stock} that you want to get the list for
+     * @param stockUUID The {@link UUID} of the {@link com.nchroniaris.mcstonks.stock.Stock} that you want to get the list for
      * @return A {@link List} of {@link Location}s, each location referencing a (hopefully still) valid SignShop sign. This will return an empty list if there are no locations in the storage OR the UUID does not exist.
      */
     @NotNull
-    List<Location> getTransactionSigns(UUID stock);
+    List<Location> getTransactionSigns(UUID stockUUID);
 
     /**
      * Gets all "history signs" for a specific stock.
      * <p>
      * A "history sign" is a sign that is responsible for displaying a number of historical prices of a specific {@link com.nchroniaris.mcstonks.stock.Stock}.
      *
-     * @param stock The {@link UUID} of the {@link com.nchroniaris.mcstonks.stock.Stock} that you want to get the list for
+     * @param stockUUID The {@link UUID} of the {@link com.nchroniaris.mcstonks.stock.Stock} that you want to get the list for
      * @return A {@link List} of {@link Location}s, each location referencing a sign (the signs pointed to by these locations are nothing special). This will return an empty list if there are no locations in the storage OR the UUID does not exist.
      */
     @NotNull
-    List<Location> getHistorySigns(UUID stock);
+    List<Location> getHistorySigns(UUID stockUUID);
 
     /**
      * Gets all the {@link com.nchroniaris.mcstonks.stock.Stock} {@link UUID}s present in the storage.
@@ -49,42 +49,42 @@ public interface StockRepository extends ConfigurationRepository {
     /**
      * Adds a transaction sign to the list for the {@link com.nchroniaris.mcstonks.stock.Stock} specified by the {@link UUID}.
      *
-     * @param stock    The {@link UUID} of the {@link com.nchroniaris.mcstonks.stock.Stock} that you want to add to
-     * @param location The {@link Location} of the Sign that you want to add. There are no checks for if this location has a valid sign.
+     * @param stockUUID The {@link UUID} of the {@link com.nchroniaris.mcstonks.stock.Stock} that you want to add to
+     * @param location  The {@link Location} of the Sign that you want to add. There are no checks for if this location has a valid sign.
      * @throws AlreadyExistsException Thrown when the provided {@link Location} is already being used.
      */
-    void addTransactionSign(UUID stock, Location location) throws AlreadyExistsException;
+    void addTransactionSign(UUID stockUUID, Location location) throws AlreadyExistsException;
 
     /**
      * Adds a history sign to the list for the {@link com.nchroniaris.mcstonks.stock.Stock} specified by the {@link UUID}.
      *
-     * @param stock    The {@link UUID} of the {@link com.nchroniaris.mcstonks.stock.Stock} that you want to add to
-     * @param location The {@link Location} of the Sign that you want to add. There are no checks for if this location has a valid sign.
+     * @param stockUUID The {@link UUID} of the {@link com.nchroniaris.mcstonks.stock.Stock} that you want to add to
+     * @param location  The {@link Location} of the Sign that you want to add. There are no checks for if this location has a valid sign.
      * @throws AlreadyExistsException Thrown when the provided {@link Location} is already being used.
      */
-    void addHistorySign(UUID stock, Location location) throws AlreadyExistsException;
+    void addHistorySign(UUID stockUUID, Location location) throws AlreadyExistsException;
 
     /**
      * Deletes a transaction sign from the list for the {@link com.nchroniaris.mcstonks.stock.Stock} specified by the {@link UUID}.
      * <p>
      * This will delete the first occurrence only (if exists), so duplicate entries (which are not technically valid) must be deleted with subsequent calls.
      *
-     * @param stock    The {@link UUID} of the {@link com.nchroniaris.mcstonks.stock.Stock} that you want to remove from
-     * @param location The {@link Location} of the Sign that you want to remove.
+     * @param stockUUID The {@link UUID} of the {@link com.nchroniaris.mcstonks.stock.Stock} that you want to remove from
+     * @param location  The {@link Location} of the Sign that you want to remove.
      * @throws DoesNotExistException Thrown when the provided {@link Location} does not exist in the storage.
      */
-    void deleteTransactionSign(UUID stock, Location location) throws DoesNotExistException;
+    void deleteTransactionSign(UUID stockUUID, Location location) throws DoesNotExistException;
 
     /**
      * Deletes a history sign from the list for the {@link com.nchroniaris.mcstonks.stock.Stock} specified by the {@link UUID}.
      * <p>
      * This will delete the first occurrence only (if exists), so duplicate entries (which are not technically valid) must be deleted with subsequent calls.
      *
-     * @param stock    The {@link UUID} of the {@link com.nchroniaris.mcstonks.stock.Stock} that you want to remove from
-     * @param location The {@link Location} of the Sign that you want to remove.
+     * @param stockUUID The {@link UUID} of the {@link com.nchroniaris.mcstonks.stock.Stock} that you want to remove from
+     * @param location  The {@link Location} of the Sign that you want to remove.
      * @throws DoesNotExistException Thrown when the provided {@link Location} does not exist in the storage.
      */
-    void deleteHistorySign(UUID stock, Location location) throws DoesNotExistException;
+    void deleteHistorySign(UUID stockUUID, Location location) throws DoesNotExistException;
 
     /**
      * Deletes a transaction sign from the list for the {@link com.nchroniaris.mcstonks.stock.Stock} specified by its {@link UUID}.
@@ -115,9 +115,9 @@ public interface StockRepository extends ConfigurationRepository {
      * <p>
      * <b>This will delete all associated transaction and history signs!</b>
      *
-     * @param stock The {@link UUID} of the entry you want to remove.
+     * @param stockUUID The {@link UUID} of the entry you want to remove.
      * @throws DoesNotExistException Thrown when the provided {@link Location} does not exist in the storage.
      */
-    void deleteStockEntry(UUID stock) throws DoesNotExistException;
+    void deleteStockEntry(UUID stockUUID) throws DoesNotExistException;
 
 }
--- src/main/java/com/nchroniaris/signstonks/repository/concrete/StockRepositoryYAML.java
+++ src/main/java/com/nchroniaris/signstonks/repository/concrete/StockRepositoryYAML.java
@@ -21,6 +21,20 @@ public class StockRepositoryYAML extends ConfigurationRepositoryYAML implements 
     private static final String NPE_MESSAGE_LOCATION = "The location field cannot be null!";
     private static final String NPE_MESSAGE_LOCATION_INVALID = "The location's world parameter cannot be null! Please construct one with a valid world.";
 
+    // -- Functional Interfaces -- //
+
+    @FunctionalInterface
+    private interface LocationListSupplier {
+        List<Location> get(UUID stockUUID);
+    }
+
+    @FunctionalInterface
+    private interface LocationListSaver {
+        void save(UUID stockUUID, List<Location> locationList);
+    }
+
+    // -- Constructors -- //
+
     /**
      * Main constructor for the Stock repository
      *
@@ -35,6 +49,8 @@ public class StockRepositoryYAML extends ConfigurationRepositoryYAML implements 
         );
 
     }
+
+    // -- Utility Methods -- //
 
     /**
      * Utility method that verifies that the given location is non-null and that the world parameter is non-null.
@@ -103,14 +119,14 @@ public class StockRepositoryYAML extends ConfigurationRepositoryYAML implements 
     /**
      * Utility method for overwriting the contents of the transaction list
      *
-     * @param stock        The {@link UUID} of the stock for which the list will be written to
+     * @param stockUUID    The {@link UUID} of the stock for which the list will be written to
      * @param locationList The list that will be written
      */
-    private void saveTransactionList(UUID stock, List<Location> locationList) {
+    private void saveTransactionList(UUID stockUUID, List<Location> locationList) {
 
         // Key: "<UUID>.transactionList"
         this.configuration.set(
-                String.join(".", stock.toString(), KEY_LIST_TRANSACTION),
+                String.join(".", stockUUID.toString(), KEY_LIST_TRANSACTION),
                 locationList
         );
 
@@ -119,41 +135,171 @@ public class StockRepositoryYAML extends ConfigurationRepositoryYAML implements 
     /**
      * Utility method for overwriting the contents of the history list
      *
-     * @param stock        The {@link UUID} of the stock for which the list will be written to
+     * @param stockUUID    The {@link UUID} of the stock for which the list will be written to
      * @param locationList The list that will be written
      */
-    private void saveHistoryList(UUID stock, List<Location> locationList) {
+    private void saveHistoryList(UUID stockUUID, List<Location> locationList) {
 
         // Key: "<UUID>.historyList"
         this.configuration.set(
-                String.join(".", stock.toString(), KEY_LIST_HISTORY),
+                String.join(".", stockUUID.toString(), KEY_LIST_HISTORY),
                 locationList
         );
 
     }
 
+    /**
+     * General method that adds a given location to a list and saves it. Specifically, it implements the following steps:
+     * <ol>
+     *     <li>Gets an existing {@link List} of {@link Location}s</li>
+     *     <li>Adds the given {@link Location} to the list</li>
+     *     <li>Saves the modified list back to the storage</li>
+     * </ol>
+     * The algorithm stays the same no matter the source of the list or the save location of the list. Therefore, we can use functional interfaces to abstract the functionality.
+     * <p>
+     * Make sure to double check the method references as you can end up overwriting the wrong list by accident.
+     *
+     * @param stockUUID The {@link UUID} of the stock you want to target. The list should be accessible from this node.
+     * @param location  The {@link Location} that you want to add
+     * @param supplier  A {@link LocationListSupplier} that supplies the {@link List} you want to modify.
+     * @param saver     A {@link LocationListSaver} that saves the resulting list to the storage.
+     * @throws AlreadyExistsException Thrown when the sign already exists somewhere in the storage.
+     */
+    private void addSignToList(UUID stockUUID, Location location, LocationListSupplier supplier, LocationListSaver saver) throws AlreadyExistsException {
+
+        Objects.requireNonNull(stockUUID, NPE_MESSAGE_UUID);
+        StockRepositoryYAML.validateLocation(location);
+
+        Objects.requireNonNull(supplier, "The supplier contract cannot be null!");
+        Objects.requireNonNull(saver, "The saver contract cannot be null!");
+
+        if (this.locationExists(location))
+            throw new AlreadyExistsException(String.format("Location %d/%d/%d/%s is already bound to a stock!",
+                    location.getBlockX(),
+                    location.getBlockY(),
+                    location.getBlockZ(),
+                    Objects.requireNonNull(location.getWorld())
+            ));
+
+        // There is no replace in-file method so we have to retrieve the list, modify it, and set it back
+        List<Location> locationList = supplier.get(stockUUID);
+        locationList.add(location);
+        saver.save(stockUUID, locationList);
+
+    }
+
+    /**
+     * General method that removes a given location from a list and saves it. Specifically, it implements the following steps:
+     * <ol>
+     *     <li>Gets an existing {@link List} of {@link Location}s</li>
+     *     <li>Deletes the given {@link Location} from the list</li>
+     *     <li>Saves the modified list back to the storage</li>
+     * </ol>
+     * The algorithm stays the same no matter the source of the list or the save location of the list. Therefore, we can use functional interfaces to abstract the functionality.
+     * <p>
+     * Make sure to double check the method references as you can end up overwriting the wrong list by accident.
+     *
+     * @param stockUUID The {@link UUID} of the stock you want to target. The list should be accessible from this node.
+     * @param location  The {@link Location} that you want to delete
+     * @param supplier  A {@link LocationListSupplier} that supplies the {@link List} you want to modify.
+     * @param saver     A {@link LocationListSaver} that saves the resulting list to the storage.
+     * @throws DoesNotExistException Thrown when the location does not exist within the source list.
+     */
+    private void deleteSignFromList(UUID stockUUID, Location location, LocationListSupplier supplier, LocationListSaver saver) throws DoesNotExistException {
+
+        Objects.requireNonNull(stockUUID, NPE_MESSAGE_UUID);
+        StockRepositoryYAML.validateLocation(location);
+
+        Objects.requireNonNull(supplier, "The supplier contract cannot be null!");
+        Objects.requireNonNull(saver, "The saver contract cannot be null!");
+
+        List<Location> locationList = supplier.get(stockUUID);
+
+        // List#remove(Object) returns false iff the object does not exist in the list, so we throw the appropriate exception if this is the case.
+        if (!locationList.remove(location))
+            throw new DoesNotExistException(String.format("Location %d/%d/%d/%s does not exist in transaction list for stock (%s)!",
+                    location.getBlockX(),
+                    location.getBlockY(),
+                    location.getBlockZ(),
+                    Objects.requireNonNull(location.getWorld()),
+                    stockUUID
+            ));
+
+        saver.save(stockUUID, locationList);
+
+    }
+
+    /**
+     * General method that removes a given location from a list and saves it. Specifically, it implements the following steps:
+     * <ol>
+     *     <li>Gets an existing {@link List} of {@link Location}s</li>
+     *     <li>Deletes the given {@link Location} from the list</li>
+     *     <li>Saves the modified list back to the storage</li>
+     *     <li>Repeats the above steps until all UUIDs in the storage have been exhausted.</li>
+     * </ol>
+     * This method is broader but slower to execute, as it needs to search through all locations saved. Useful if you don't have a UUID.
+     * <p>
+     * The algorithm stays the same no matter the source of the list or the save location of the list. Therefore, we can use functional interfaces to abstract the functionality.
+     * <p>
+     * Make sure to double check the method references as you can end up overwriting the wrong list by accident.
+     *
+     * @param location The {@link Location} that you want to delete
+     * @param supplier A {@link LocationListSupplier} that supplies the {@link List} you want to modify.
+     * @param saver    A {@link LocationListSaver} that saves the resulting list to the storage.
+     * @throws DoesNotExistException Thrown when the location does not exist within the entire storage.
+     */
+    private void deleteSignFromList(Location location, LocationListSupplier supplier, LocationListSaver saver) throws DoesNotExistException {
+
+        for (UUID stockUUID : this.getStockEntries()) {
+
+            try {
+
+                // If the deletion succeeds for this UUID, no error will be thrown and the return path will be taken, thus not throwing an error
+                this.deleteSignFromList(stockUUID, location, supplier, saver);
+                return;
+
+            } catch (DoesNotExistException ignored) {
+
+                // We are ignoring this because it is possible that the element may exist under a different UUID's list, and we don't want to prematurely error out.
+
+            }
+
+        }
+
+        // If execution arrives here, that means that every UUID's list has been exhausted and the location has not been found anywhere. To abide by the method contract, we throw a final error to indicate it wasn't found anywhere.
+        throw new DoesNotExistException(String.format("Location %d/%d/%d/%s does not exist in any stock's transaction list!",
+                location.getBlockX(),
+                location.getBlockY(),
+                location.getBlockZ(),
+                Objects.requireNonNull(location.getWorld())
+        ));
+
+    }
+
+    // -- Public Methods -- //
+
     @Override
     @NotNull
-    public synchronized List<Location> getTransactionSigns(UUID stock) {
+    public synchronized List<Location> getTransactionSigns(UUID stockUUID) {
 
-        Objects.requireNonNull(stock, NPE_MESSAGE_UUID);
+        Objects.requireNonNull(stockUUID, NPE_MESSAGE_UUID);
 
         // Key: "<UUID>.transactionList"
         return this.getLocationList(
-                this.configuration.getList(String.join(".", stock.toString(), KEY_LIST_TRANSACTION))
+                this.configuration.getList(String.join(".", stockUUID.toString(), KEY_LIST_TRANSACTION))
         );
 
     }
 
     @Override
     @NotNull
-    public synchronized List<Location> getHistorySigns(UUID stock) {
+    public synchronized List<Location> getHistorySigns(UUID stockUUID) {
 
-        Objects.requireNonNull(stock, NPE_MESSAGE_UUID);
+        Objects.requireNonNull(stockUUID, NPE_MESSAGE_UUID);
 
         // Key: "<UUID>.historyList"
         return this.getLocationList(
-                this.configuration.getList(String.join(".", stock.toString(), KEY_LIST_HISTORY))
+                this.configuration.getList(String.join(".", stockUUID.toString(), KEY_LIST_HISTORY))
         );
 
     }
@@ -186,59 +332,85 @@ public class StockRepositoryYAML extends ConfigurationRepositoryYAML implements 
     }
 
     @Override
-    public synchronized void addTransactionSign(UUID stock, Location location) throws AlreadyExistsException {
+    public synchronized void addTransactionSign(UUID stockUUID, Location location) throws AlreadyExistsException {
 
-        Objects.requireNonNull(stock, NPE_MESSAGE_UUID);
-        StockRepositoryYAML.validateLocation(location);
-
-        if (this.locationExists(location))
-            throw new AlreadyExistsException(String.format("Location %d/%d/%d/%s is already bound to a stock!", location.getBlockX(), location.getBlockY(), location.getBlockZ(), Objects.requireNonNull(location.getWorld())));
-
-        // There is no replace in-file method so we have to retrieve the list, modify it, and set it back
-        List<Location> locationList = this.getTransactionSigns(stock);
-        locationList.add(location);
-        this.saveTransactionList(stock, locationList);
+        this.addSignToList(
+                stockUUID,
+                location,
+                this::getTransactionSigns,
+                this::saveTransactionList
+        );
 
     }
 
     @Override
-    public synchronized void addHistorySign(UUID stock, Location location) throws AlreadyExistsException {
+    public synchronized void addHistorySign(UUID stockUUID, Location location) throws AlreadyExistsException {
 
-        Objects.requireNonNull(stock, NPE_MESSAGE_UUID);
-        StockRepositoryYAML.validateLocation(location);
-
-        if (this.locationExists(location))
-            throw new AlreadyExistsException(String.format("Location %d/%d/%d/%s is already bound to a stock!", location.getBlockX(), location.getBlockY(), location.getBlockZ(), Objects.requireNonNull(location.getWorld())));
-
-        // There is no replace in-file method so we have to retrieve the list, modify it, and set it back
-        List<Location> locationList = this.getHistorySigns(stock);
-        locationList.add(location);
-        this.saveHistoryList(stock, locationList);
+        this.addSignToList(
+                stockUUID,
+                location,
+                this::getHistorySigns,
+                this::saveHistoryList
+        );
 
     }
 
     @Override
-    public void deleteTransactionSign(UUID stock, Location location) throws DoesNotExistException {
+    public synchronized void deleteTransactionSign(UUID stockUUID, Location location) throws DoesNotExistException {
+
+        this.deleteSignFromList(
+                stockUUID,
+                location,
+                this::getTransactionSigns,
+                this::saveTransactionList
+        );
 
     }
 
     @Override
-    public void deleteHistorySign(UUID stock, Location location) throws DoesNotExistException {
+    public synchronized void deleteHistorySign(UUID stockUUID, Location location) throws DoesNotExistException {
+
+        this.deleteSignFromList(
+                stockUUID,
+                location,
+                this::getHistorySigns,
+                this::saveHistoryList
+        );
 
     }
 
     @Override
-    public void deleteTransactionSign(Location location) throws DoesNotExistException {
+    public synchronized void deleteTransactionSign(Location location) throws DoesNotExistException {
+
+        this.deleteSignFromList(
+                location,
+                this::getTransactionSigns,
+                this::saveTransactionList
+        );
 
     }
 
     @Override
-    public void deleteHistorySign(Location location) throws DoesNotExistException {
+    public synchronized void deleteHistorySign(Location location) throws DoesNotExistException {
+
+        this.deleteSignFromList(
+                location,
+                this::getHistorySigns,
+                this::saveHistoryList
+        );
 
     }
 
     @Override
-    public void deleteStockEntry(UUID stock) throws DoesNotExistException {
+    public synchronized void deleteStockEntry(UUID stockUUID) throws DoesNotExistException {
+
+        Objects.requireNonNull(stockUUID, NPE_MESSAGE_UUID);
+
+        if (!this.configuration.contains(stockUUID.toString(), false))
+            throw new DoesNotExistException(String.format("The UUID '%s' is not present in the storage!", stockUUID));
+
+        // Clear entire ConfigurationSection
+        this.configuration.set(stockUUID.toString(), null);
 
     }
 
